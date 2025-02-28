8afdf1395c682ce48c88c0c772ec9f1c633c979b
julesferguson456 <julesferguson456@gmail.com>
2025-02-28 17:07:33 -0500

    first commit

---
+++ app.js
@@ -0,0 +1,54 @@
+// I used ChatGPT to check all of my examples. I will write how far I got on my own and compate with ChatGPT. 
+
+// function isAdult (21,) {
+//     if (21 > 18) {
+//         return Adult
+//     } else (18) {
+//         return Minor
+//     }
+// }
+// console.log(isAdult)
+
+
+// Chat GPT correct answer = 
+
+// function isAdult(age) {
+//     if (age > 18) {
+//         return "Adult";
+//     } else {
+//         return "Minor";
+//     }
+// }
+
+// // console.log(isAdult(21))
+
+
+
+
+
+
+
+
+/*
+Exercise 3: isCharAVowel()
+
+Write a function named isCharAVowel that takes a single character as 
+an argument. It should return true if the character is a vowel and 
+false otherwise. For the purposes of this exercise, the character 
+y should not be considered a vowel.
+
+Example: isCharAVowel('a') should return true.
+
+Complete the exercise in the space below:
+*/
+
+function isCharAVowel("a") {
+
+}
+if (isCharAVowel = vowel)
+    return true
+else {
+    return false
+}
+
+console.log('Exercise 3 Result:', isCharAVowel("a"));
